641d3a135bce01337b22a128dff8858ca97e4334
Abulele75 <ntwanambiabulele@gmail.com>
2024-12-10 18:07:35 +0200

    added basic search engine functionality

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function Search(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#input-city");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= searchInput.value;
+
+}
+
 let searchForm = document.querySelector("#search-form");
-console.log(searchForm);
+searchForm.addEventListener("submit" , Search);
 
