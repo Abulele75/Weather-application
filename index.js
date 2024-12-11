9a52dad402531ddcd0a1ba91b28c221bc8c0521f
Abulele75 <ntwanambiabulele@gmail.com>
2024-12-12 00:28:50 +0200

    adjusted css styling

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+
+
 function Search(event){
     event.preventDefault();
+
     let searchInput = document.querySelector("#input-city");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML= searchInput.value;
