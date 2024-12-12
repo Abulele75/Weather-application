f00cbf45ae5cd7ded663f38abf1e98cbf0a2c7af
Abulele75 <ntwanambiabulele@gmail.com>
2024-12-12 02:15:43 +0200

    Updated JS to show current weather

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+function UpdateWeather(response){
+    let temperature = document.querySelector("#weather-temperature")
+    temperature.innerHTML=response.data.temperature.current.
+    console.log(response.data.current.temperature.current);
 
+}
+
+
+
+function SearchCity (city) {
+let apiKey = "4f430cb68t0bf0b27o781c38a281438d"
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+axios.get(apiUrl).then(UpdateWeather);
+}
 
 function Search(event){
     event.preventDefault();
@@ -6,7 +19,7 @@ function Search(event){
     let searchInput = document.querySelector("#input-city");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML= searchInput.value;
-
+SearchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
