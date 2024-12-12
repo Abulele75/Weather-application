84f6c331e2dc3567da1adc95e7e8a7831524d6a6
Abulele75 <ntwanambiabulele@gmail.com>
2024-12-12 02:53:30 +0200

    Added a search functionality using Api

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 function UpdateWeather(response){
-    let temperature = document.querySelector("#weather-temperature")
-    temperature.innerHTML=response.data.temperature.current.
-    console.log(response.data.current.temperature.current);
-
+    let temperature = document.querySelector("#weather-temperature");
+    
+    let temperatureElement=response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML= response.data.city;
+   temperature.innerHTML = Math.round(temperatureElement);
 }
 
 
 
 function SearchCity (city) {
 let apiKey = "4f430cb68t0bf0b27o781c38a281438d"
-let apiUrl = "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
 axios.get(apiUrl).then(UpdateWeather);
 }
 
@@ -17,11 +19,11 @@ function Search(event){
     event.preventDefault();
 
     let searchInput = document.querySelector("#input-city");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML= searchInput.value;
+
 SearchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit" , Search);
 
+SearchCity("Durban");
