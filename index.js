8c19159ff01e7fd362c7b5974f3b54482fbf1e86
Abulele75 <ntwanambiabulele@gmail.com>
2024-12-15 02:51:28 +0200

    Added more weather data

--- index.js
+++ index.js
@@ -4,13 +4,36 @@ function UpdateWeather(response){
     let temperatureElement=response.data.temperature.current;
     let cityElement = document.querySelector("#city");
     let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#Humidity");
+    let windSpeedElement = document.querySelector("#windSpeed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date ( response.data.time * 1000)
+
 
     
-
+    
     cityElement.innerHTML= response.data.city;
+    timeElement.innerHTML= formatDate(date);
    descriptionElement.innerHTML = response.data.condition.description;
+
+   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
    temperature.innerHTML = Math.round(temperatureElement);
 }
+
+function formatDate (date){
+    
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if (minutes <10){
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes}`;
+}
+
 
 
 
